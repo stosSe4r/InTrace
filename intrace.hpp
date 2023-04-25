40b560915dc0c2b96d37b3e4786bc912ac447df1
Nobody <nobody@whoami.fun>
2023-04-25 22:03:18 +0800

    Declare a helper class.

--- intrace.hpp
+++ intrace.hpp
@@ -1,17 +1,24 @@
 #ifndef InTrace_H
 #define InTrace_H
 
-#include<stack>
+#include<deque>
 #include<string>
 #include<chrono>
 
 struct _it_func_call {
-    std::string func; // function name
+    std::string fn; // function name
     std::chrono::time_point<std::chrono::steady_clock> prolog, epilog; // the time point entering and exiting the function
 };
 
 struct {
-    std::stack<_it_func_call> cs; // call stack
+    std::deque<_it_func_call> cs; // call stack
 } _InTrace_data;
+
+class _InTracer {
+    std::string fn;
+public:
+    _InTracer(std::string);
+    ~_InTracer();
+};
 
 #endif
